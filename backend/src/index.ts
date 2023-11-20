05e53e67828a93cb3b5aa62b7f0996352478cac2
Laynaria <pro.laynaria@outlook.fr>
2023-11-20 11:24:22 +0100

    script to install all modules in backend, frontend and global + gitignore in frontend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,7 @@ import { buildSchema } from "type-graphql";
 import { ApolloServer } from "apollo-server";
 import { dataSource } from "./config/db";
 
+// to delete
 const typeDefs = `#graphql
   # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
 
@@ -36,6 +37,8 @@ const resolvers = {
     books: () => books,
   },
 };
+
+//end to delete
 
 const port: number = 5000;
 
